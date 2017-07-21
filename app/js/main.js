d42e6ae2fe9a68227122fee25ebbce71e5e85b30
Martin Sandström <martin.sandstrom@postkodlotteriet.se>
2017-07-21 16:34:03 +0200

    First commit: Setting up everything

---
+++ app/js/main.js
@@ -0,0 +1,53 @@
+var application = new Vue({
+    el: '#application',
+    data: {
+        message: 'Hello Vue!',
+        drinks: [
+            {
+                idDrink: '12708',
+                strDrink: 'Banana Cantaloupe Smoothie',
+                strCategory: 'Other/Unknown',
+                strAlcoholic: 'Non alcoholic',
+                strGlass: 'vote',
+                strInstructions: 'Juice cantaloupe, pour juice into blender, add banana, and liquify.',
+                strDrinkThumb: 'http://www.thecocktaildb.com/images/media/drink/uqxqsy1468876703.jpg',
+                strIngredient1: 'Cantaloupe',
+                strIngredient2: 'Banana',
+                strIngredient3: '',
+                strIngredient4: '',
+                strIngredient5: '',
+                strIngredient6: '',
+                strIngredient7: '',
+                strIngredient8: '',
+                strIngredient9: '',
+                strIngredient10: '',
+                strIngredient11: '',
+                strIngredient12: '',
+                strIngredient13: '',
+                strIngredient14: '',
+                strIngredient15: '',
+                strMeasure1: 'Juice of 1/2 ',
+                strMeasure2: '1',
+                strMeasure3: '',
+                strMeasure4: '',
+                strMeasure5: '',
+                strMeasure6: '',
+                strMeasure7: '',
+                strMeasure8: '',
+                strMeasure9: '',
+                strMeasure10: '',
+                strMeasure11: '',
+                strMeasure12: '',
+                strMeasure13: '',
+                strMeasure14: '',
+                strMeasure15: '',
+                dateModified: '2016-07-18 22:18:23'
+            }
+        ]
+    },
+    methods: {
+        getNewDrinks: function() {
+            this.message = 'Ye right I am the new kid on the block';
+        }
+    }
+});
